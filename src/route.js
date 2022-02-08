492a86f6aadaf9a24e273b70a8cab32a4ea8b754
ManoelRIos <manoel.rios@cfc.org.br>
2022-02-08 15:48:30 -0300

    Adicionando rotas

--- src/route.js
+++ src/route.js
@@ -3,6 +3,8 @@ const express = require('express')
 const route = express.Router()
 
 route.get('/', (req, res) => res.render("index"))
+route.get('/room', (req, res) => res.render("room"))
+route.get('/create-room', (req, res) => res.render("create-room"))
 
 
 module.exports = route
