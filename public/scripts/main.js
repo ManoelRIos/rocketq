776a174afa3b65c1fb8e03dd21e0bb15ac36c7c6
ManoelRIos <manoel.rios@cfc.org.br>
2022-02-10 16:41:59 -0300

    Adicionando lógica no modal ao clicar em excluir ou marcar como lida

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,13 +2,28 @@ import  Modal  from './modal.js'
 
 const modal = Modal()
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 //Pegar todos botões que existem com class check
 const checkButtons = document.querySelectorAll(".actions a.check")
 
 checkButtons.forEach(button =>{
-  button.addEventListener("click", event =>{
-    console.log('click')
-    modal.open()
-  })
+  button.addEventListener("click", handleClick)
 })
 
+const deleteButton = document.querySelectorAll(".actions .delete")
+
+deleteButton.forEach(button =>{
+  button.addEventListener("click", (event) => handleClick(event, false))
+})
+
+function handleClick(event, check = true ){
+  const text = check ? "Marcar como lida" : "Excluir"
+  modalTitle.innerHTML = `${text} esta pergunta`
+  modalDescription.innerHTML =`Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+
+  //Abrir modal
+  modal.open()
+}
