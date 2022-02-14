7020b342a1a197b99c9f6e9e5eb1c0a0999c31de
ManoelRIos <manoel.rios@cfc.org.br>
2022-02-14 15:49:19 -0300

    Adicionando funcionalidade de abertura do modal ao clicar em  ou

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -20,9 +20,21 @@ deleteButton.forEach(button =>{
 })
 
 function handleClick(event, check = true ){
+  //enviar a passagem do "a" para url
+  event.preventDefault()
+
   const text = check ? "Marcar como lida" : "Excluir"
+  const slug = check ? "check" : "delete"
+  const roomId = document.querySelector("#room-id").dataset.id
+  const questionId = event.target.dataset.id
+  const form = document.querySelector(".modal form")
+
+  form.setAttribute("action" , `/room/${roomId}/${questionId}/${slug}`)
+
   modalTitle.innerHTML = `${text} esta pergunta`
   modalDescription.innerHTML =`Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+  modalButton.innerHTML= `Sim, ${text.toLowerCase()}`
+  check? modalButton.classList.remove("red") : modalButton.classList.add("red")
 
   //Abrir modal
   modal.open()
