e92d7e8f4c77a1d445755c468615014f2b7a6a5b
ManoelRIos <manoel.rios@cfc.org.br>
2022-02-14 16:18:45 -0300

    Refatorando html duplicado no ejs, adicionando rotas dinâmicas para criação de salas

--- src/server.js
+++ src/server.js
@@ -4,9 +4,11 @@ const path = require('path')
 
 const server = express()
 
-
 server.set('view engine', 'ejs')
-server.set('views', path.join(__dirname, 'views') )
+server.set('views', path.join(__dirname, 'views'))
+
+server.use(express.urlencoded({extended: true}))
 server.use(express.static("public"))
 server.use(route)
+
 server.listen(3000, () => console.log("Rodando na porta 3000"))
