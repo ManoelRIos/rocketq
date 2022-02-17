5958898902f5416efdec455ab33822f7576f82a1
ManoelRIos <manoel.rios@cfc.org.br>
2022-02-17 17:10:29 -0300

    Adicionando lógica para o id nunca salvar id no bd

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,28 +1,38 @@
-const Database = require("../db/config")
+const Database = require("../db/config");
 
 module.exports = {
-  async create(req, res){
-    const db = await Database()
-    const pass = req.body.password
-    let roomId
-
-    for(var i = 0; i < 6; i++){
-      i == 0 ? roomId = Math.floor(Math.random() * 10).toString(): 
-      roomId += Math.floor(Math.random() * 10).toString()
+  async create(req, res) {
+    const db = await Database();
+    const pass = req.body.password;
+    let roomId;
+    let isRoom = true;
+    
+    while (isRoom) {
+      for (var i = 0; i < 6; i++) {
+        i == 0
+          ? (roomId = Math.floor(Math.random() * 10).toString())
+          : (roomId += Math.floor(Math.random() * 10).toString());
+      }
+  
+      const roomsExistIds = await db.all(`SELECT id FROM rooms`);
+  
+      let isRoom = roomsExistIds.some((roomExistIds) => roomExistIds === roomId);
+  
+      if (!isRoom) {
+        await db.run(`INSERT INTO rooms (
+          id,
+          pass
+        )VALUES(
+          ${parseInt(roomId)},
+          ${pass}
+        )`);
+      }
     }
 
 
-    await db.run(`INSERT INTO rooms (
-      id,
-      pass
-    )VALUES(
-      ${parseInt(roomId)},
-      ${pass}
-    )`)
 
-    await db.close()
+    await db.close();
 
-    res.redirect(`/room/${roomId}`)
-
-  }
-}
+    res.redirect(`/room/${roomId}`);
+  },
+};
