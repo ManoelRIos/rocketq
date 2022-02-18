8c9c20d15fdbd5435ba4c1cb7ccd20781b99cfdd
ManoelRIos <manoel.rios@cfc.org.br>
2022-02-18 15:57:57 -0300

    Corrigindo erro de lógica de não repetir mesmo id para o rooom

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -6,18 +6,16 @@ module.exports = {
     const pass = req.body.password;
     let roomId;
     let isRoom = true;
+
     
     while (isRoom) {
       for (var i = 0; i < 6; i++) {
-        i == 0
-          ? (roomId = Math.floor(Math.random() * 10).toString())
-          : (roomId += Math.floor(Math.random() * 10).toString());
+        i == 0 ? (roomId = Math.floor(Math.random() * 10).toString()) :
+        (roomId += Math.floor(Math.random() * 10).toString());
       }
   
       const roomsExistIds = await db.all(`SELECT id FROM rooms`);
-  
-      let isRoom = roomsExistIds.some((roomExistIds) => roomExistIds === roomId);
-  
+      isRoom = roomsExistIds.some((roomExistIds) => roomExistIds === roomId);
       if (!isRoom) {
         await db.run(`INSERT INTO rooms (
           id,
@@ -25,14 +23,16 @@ module.exports = {
         )VALUES(
           ${parseInt(roomId)},
           ${pass}
-        )`);
+        )`)
       }
     }
-
-
-
     await db.close();
 
     res.redirect(`/room/${roomId}`);
   },
-};
+
+  open(req,res){
+    const roomId = req.params.room
+    res.render("room", {roomId: roomId})
+  }
+}
